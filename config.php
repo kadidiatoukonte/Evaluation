3bb4fabb0a30e19dfb07baaecf535932ad103d96
kadidiatoukonte <kontekadidiatou@yahoo.fr>
2018-10-30 11:27:07 +0100

    Add wireframe schema and use case

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ try
 
 {
 
-    $bdd = new PDO('mysql:host=localhost;dbname=evaluation;charset=utf8', 'root', 'aoua1993');
+    $bdd = new PDO('mysql:host=localhost;dbname=evaluation;charset=utf8', 'root', '');
 
 }
 
